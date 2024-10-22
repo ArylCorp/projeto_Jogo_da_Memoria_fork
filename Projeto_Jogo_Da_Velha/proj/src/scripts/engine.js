6f74b915ce4a2a4688dbd0a259de4edc9806ecb2
LLA3004 <lucas10lyra@gmail.com>
2024-10-22 02:36:21 +0000

    modified:   Projeto_Jogo_Da_Velha/proj/src/scripts/engine.js 	modified:   Projeto_Jogo_Da_Velha/proj/src/styles/main.css

--- Projeto_Jogo_Da_Velha/proj/src/scripts/engine.js
+++ Projeto_Jogo_Da_Velha/proj/src/scripts/engine.js
@@ -17,6 +17,29 @@ let embaralhaEmoji = emojis.sort(()=>(Math.random() >0.5 ? 2 : -1))
 for(let i=0; i<emojis.length; i++){
     let box = document.createElement("div")
     box.className ="item"
+    box.onclick = handleClick
     box.innerHTML = embaralhaEmoji[i]
     document.querySelector(".game").appendChild(box)
+}
+
+function handleClick(){
+    if(cartaAberta.length < 2){
+        this.classList.add("caixaAberta")
+        cartaAberta.push(this)
+
+        if(cartaAberta.length == 2){
+            setTimeout(checaCartas, 500)
+        }
+    }
+}
+
+function checaCartas(){
+    if(cartaAberta[0].innerHTML === cartaAberta[1].innerHTML){
+        cartaAberta[0].classList.add("boxMatch")
+        cartaAberta[1].classList.add("boxMatch")
+    } else{
+        cartaAberta[0].classList.remove("caixaAberta")
+        cartaAberta[1].classList.remove("caixaAberta")
+    }
+    cartaAberta = []
 }
